ffa0b7d38f2493cf3629dc834afe9b400861e405
Ellen <ellen.bacerra@gmail.com>
2018-08-08 10:44:00 -0500

    update

---
+++ week-3/99-other-activities/functions.js
@@ -0,0 +1,17 @@
+function difference(num1, num2) {
+    return num1 - num2;
+}
+console.log(difference(1, 0));
+
+
+function wholeName(first, middle, last) {
+    return first " " + middle " " + last " ";
+}
+wholeNamce("Ellen," "Corpuz," "Bacerra");
+console.log(result);
+
+
+function string(aString) {
+    return string.split("").reversed().join("");
+
+}
